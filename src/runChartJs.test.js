e989cc189a62c4736c3ac55d3d418a9b69091cd9
mstssk <mstssk+github@gmail.com>
2021-10-06 00:52:07 +0900

    Comments

--- src/runChartJs.test.js
+++ src/runChartJs.test.js
@@ -1,7 +1,17 @@
 const { runChartJs } = require("./runChartJs");
 
+/*
+ * `jest.mock("chart.js")` does mock also "chart.js/helpers" module.
+ * There are no way to use actual "chart.js/helpers" module if mock "chart.js" module.
+ * `jest.unmock("chart.js/helpers")` does not work.
+ */
 jest.mock("chart.js").mock("chart.js/helpers", () => {
-  return jest.requireActual("chart.js/helpers");
+  // 😕 'requireActual' returns mocked module, not actual.
+  const orig = jest.requireActual("chart.js/helpers");
+  return {
+    __esModule: true,
+    ...orig,
+  };
 });
 
 test("test", () => {
